840cd418244173b3a9cd2a64e57d76cde552dd71
jeshio <jeshio@yandex.ru>
2014-06-22 12:46:55 +0600

    The first and second exercices

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-  	@title = 'Home';
   end
 
   def help
-  	@title = 'Help';
   end
 
   def about
-  	@title = 'About';
+  end
+
+  def contact
   end
 end
