6494ee28a67481a8279569bc3fd126ee48aca480
jeshio <jeshio@yandex.ru>
2014-07-10 22:28:03 +0600

    Edit page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,6 +12,23 @@ describe "UserPages" do
 
     end
 
+    describe "edit" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit edit_user_path(user) }
+
+        describe "page" do
+            it { should have_content('Update your profile') }
+            it { should have_title('Edit user') }
+            it { should have_link('change', href: 'http://gravatar.com/emails') }
+        end
+
+        describe "with invalid information" do
+            before { click_button "Save changes" }
+
+            it { should have_content('error') }
+        end
+    end
+
     describe "signup page" do
     	before { visit signup_path }
         let(:submit) { "Create my account" }
